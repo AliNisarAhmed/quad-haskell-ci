c3f08f08f2e7d369edd678b8d4c552cf9e6d8031
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-05-21 12:54:40 -0600

    Parsing JSON response by docker API to get container Id

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,25 +1,52 @@
 module Docker where
 
-import qualified Network.HTTP.Simple as HTTP
+import Data.Aeson ((.:))
 import qualified Data.Aeson as Aeson
-import qualified Socket
+import qualified Data.Aeson.Types as Aeson.Types
+import qualified Network.HTTP.Simple as HTTP
 import RIO
+import qualified Socket
 
 data CreateContainerOptions = CreateContainerOptions {image :: Image}
 
-createContainer :: CreateContainerOptions -> IO ()
+newtype ContainerId = ContainerId Text
+  deriving (Eq, Show)
+
+containerIdToText :: ContainerId -> Text
+containerIdToText (ContainerId c) = c
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
   manager <- Socket.newManager "/var/run/docker.sock"
-  let body = Aeson.Null
+  let image = imageToText options.image
+  let body =
+        Aeson.object
+          [ ("Image", Aeson.toJSON image),
+            ("Tty", Aeson.toJSON True),
+            ("Labels", Aeson.object [("quad", "")]),
+            ("Cmd", "echo hello"),
+            ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
+          ]
   let req =
         HTTP.defaultRequest
           & HTTP.setRequestManager manager
           & HTTP.setRequestPath "/v1.40/containers/create"
           & HTTP.setRequestMethod "POST"
           & HTTP.setRequestBodyJSON body
+  let parser = Aeson.withObject "create-container" $ \o -> do
+        cId <- o .: "Id"
+        pure $ ContainerId cId
   res <- HTTP.httpBS req
-  -- dump the response to stdout
-  traceShowIO res
+  parseResponse res parser
+
+parseResponse :: HTTP.Response ByteString -> (Aeson.Value -> Aeson.Types.Parser a) -> IO a
+parseResponse res parser = do
+  let result = do
+        value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+        Aeson.Types.parseEither parser value
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
 
 newtype Image = Image Text
   deriving (Eq, Show)
