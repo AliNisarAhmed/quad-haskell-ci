bf0ed919cb0a63f16d10b2b7f1a5ad2bb3878808
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-05-22 09:45:03 -0600

    extract build runs into a separate module, add a failing test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,15 +5,19 @@ import qualified Docker
 import RIO
 import qualified RIO.Map as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import qualified Runner
 import qualified System.Process.Typed as Process
 import Test.Hspec
 
 main :: IO ()
 main = hspec do
   docker <- runIO Docker.createService
+  runner <- runIO $ Runner.createService docker
   beforeAll cleanupDocker $ describe "Quad CI" do
     it "should run a build (success)" do
-      testRunSuccess docker
+      testRunSuccess runner
+    it "should run a build (failure)" do
+      testRunFailure runner
 
 cleanupDocker :: IO ()
 cleanupDocker = void do
@@ -42,26 +46,26 @@ makePipeline :: [Step] -> Pipeline
 makePipeline steps =
   Pipeline {steps = NonEmpty.Partial.fromList steps}
 
--- Test values
-testPipeline :: Pipeline
-testPipeline =
-  makePipeline
-    [ makeStep "First Step" "ubuntu" ["date"],
-      makeStep "Second step" "ubuntu" ["uname -r"]
-    ]
-
-testBuild :: Build
-testBuild =
-  Build
-    { pipeline = testPipeline,
-      state = BuildReady,
-      completedSteps = mempty
-    }
-
 -- first test
 
-testRunSuccess :: Docker.Service -> IO ()
-testRunSuccess docker = do
-  result <- runBuild docker testBuild
+testRunSuccess :: Runner.Service -> IO ()
+testRunSuccess runner = do
+  build <-
+    runner.prepareBuild $
+      makePipeline
+        [ makeStep "First Step" "ubuntu" ["date"],
+          makeStep "Second Step" "ubuntu" ["uname -r"]
+        ]
+  result <- runner.runBuild build
   result.state `shouldBe` BuildFinished BuildSucceeded
   Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
+
+testRunFailure :: Runner.Service -> IO ()
+testRunFailure runner = do
+  build <-
+    runner.prepareBuild $
+      makePipeline
+        [makeStep "Should Fail" "ubuntu" ["exit 1"]]
+  result <- runner.runBuild build
+  result.state `shouldBe` BuildFinished BuildFailed
+  Map.elems result.completedSteps `shouldBe` [StepFailed (Docker.ContainerExitCode 1)]
