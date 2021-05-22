95fd52ea9ee23678cb553c6ac5a04cdd5659b9bf
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-05-22 08:27:54 -0600

    set up tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,32 @@
 module Main where
 
 import Core
-import RIO
-import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
 import qualified Docker
+import RIO
+import qualified RIO.Map as Map
+import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import qualified System.Process.Typed as Process
+import Test.Hspec
+
+main :: IO ()
+main = hspec do
+  docker <- runIO Docker.createService
+  beforeAll cleanupDocker $ describe "Quad CI" do
+    it "should run a build (success)" do
+      testRunSuccess docker
+
+cleanupDocker :: IO ()
+cleanupDocker = void do
+  Process.readProcessStdout "docker rm -f $(docker ps -aq --filter \"label=quad\")"
+
+runBuild :: Docker.Service -> Build -> IO Build
+runBuild docker build = do
+  newBuild <- Core.progress docker build
+  case newBuild.state of
+    BuildFinished _ -> pure newBuild
+    _ -> do
+      threadDelay (1 * 1000 * 1000)
+      runBuild docker newBuild
 
 -- Helper Functions
 
@@ -35,5 +58,10 @@ testBuild =
       completedSteps = mempty
     }
 
-main :: IO ()
-main = pure ()
+-- first test
+
+testRunSuccess :: Docker.Service -> IO ()
+testRunSuccess docker = do
+  result <- runBuild docker testBuild
+  result.state `shouldBe` BuildFinished BuildSucceeded
+  Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
