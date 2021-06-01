9384a27bede57633058f483d941b73b4d475fc71
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-05-31 20:33:33 -0600

    building the agent

--- src/JobHandler.hs
+++ src/JobHandler.hs
@@ -1,15 +1,14 @@
 module JobHandler where
 
-import RIO
-import Core
 import qualified Agent
+import Core
+import RIO
 
-data Job
-  = Job
-      { pipeline :: Pipeline
-      , state :: JobState
-      }
-    deriving (Eq, Show)
+data Job = Job
+  { pipeline :: Pipeline,
+    state :: JobState
+  }
+  deriving (Eq, Show)
 
 data JobState
   = JobQueued
@@ -17,8 +16,8 @@ data JobState
   | JobScheduled Build
   deriving (Eq, Show)
 
-data Service
-  = Service { queueJob :: Pipeline -> IO BuildNumber
-            , dispatchCmd :: IO (Maybe Agent.Cmd)
-            , processMsg :: Agent.Msg -> IO ()
-            }
+data Service = Service
+  { queueJob :: Pipeline -> IO BuildNumber,
+    dispatchCmd :: IO (Maybe Agent.Cmd),
+    processMsg :: Agent.Msg -> IO ()
+  }
