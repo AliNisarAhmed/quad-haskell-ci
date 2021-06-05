c4df3cab5f5e9aa609ff0794c6e24a28f98076ec
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-06-05 15:23:58 -0600

    building api for the web UI

--- src/Server.hs
+++ src/Server.hs
@@ -5,7 +5,9 @@ import Core
 import qualified Data.Aeson as Aeson
 import qualified Github
 import qualified JobHandler
+import qualified Network.HTTP.Types as HTTP.Types
 import RIO
+import qualified RIO.Map as Map
 import qualified RIO.NonEmpty as NonEmpty
 import qualified Web.Scotty as Scotty
 
@@ -37,3 +39,67 @@ run config handler =
         let step = Github.createCloneStep info
         handler.queueJob $ pipeline {steps = NonEmpty.cons step pipeline.steps}
       Scotty.json $ Aeson.object [("number", Aeson.toJSON $ Core.buildNumberToInt number), ("status", "job queued")]
+
+    Scotty.get "/build/:number" do
+      number <- BuildNumber <$> Scotty.param "number"
+
+      job <-
+        Scotty.liftAndCatchIO
+          (handler.findJob number)
+          >>= \case
+            Nothing -> Scotty.raiseStatus HTTP.Types.status404 "Build not found"
+            Just j -> pure j
+      Scotty.json $ jobToJson number job
+
+jobToJson :: BuildNumber -> JobHandler.Job -> Aeson.Value
+jobToJson number job =
+  Aeson.object
+    [ ("number", Aeson.toJSON $ Core.buildNumberToInt number),
+      ("state", Aeson.toJSON $ jobStateToText job.state),
+      ("steps", Aeson.toJSON steps)
+    ]
+  where
+    build = case job.state of
+      JobHandler.JobQueued -> Nothing
+      JobHandler.JobAssigned -> Nothing
+      JobHandler.JobScheduled b -> Just b
+    steps =
+      job.pipeline.steps <&> \step ->
+        Aeson.object
+          [ ("name", Aeson.String $ Core.stepNameToText step.name),
+            ( "state",
+              Aeson.String $ case build of
+                Just b -> stepStateToText b step
+                Nothing -> "ready"
+            )
+          ]
+
+jobStateToText :: JobHandler.JobState -> Text
+jobStateToText = \case
+  JobHandler.JobQueued -> "queued"
+  JobHandler.JobAssigned -> "assigned"
+  JobHandler.JobScheduled b -> case b.state of
+    BuildReady -> "ready"
+    BuildRunning _ -> "running"
+    BuildFinished result ->
+      case result of
+        BuildSucceeded -> "succeeded"
+        BuildFailed -> "failed"
+        BuildUnexpectedState _ -> "unexpectedstate"
+
+stepStateToText :: Build -> Step -> Text
+stepStateToText build step =
+  case build.state of
+    BuildRunning s ->
+      if s.step == step.name
+        then "running"
+        else stepNotRunning
+    _ -> stepNotRunning
+  where
+    stepNotRunning =
+      case Map.lookup step.name build.completedSteps of
+        Just StepSucceeded -> "succeeded"
+        Just (StepFailed _) -> "failed"
+        Nothing -> case build.state of
+          BuildFinished _ -> "skipped"
+          _ -> "ready"
